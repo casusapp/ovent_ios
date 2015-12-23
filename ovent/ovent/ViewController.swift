f8cc26bf53d4b1c1de90c6c1da94291f68fa30c3
Rushabh <rushabh@Rushabhs-MacBook-Pro.local>
2015-12-23 22:29:12 +0530

    pods created

---
+++ ovent/ovent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ovent
+//
+//  Created by Rushabh on 20/12/15.
+//  Copyright © 2015 Rushabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
